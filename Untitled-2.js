d6fbcc44a22e084d30f7938f55ff815a4551845b
GabrielTemito <gabriel.sousa.gf63@gmail.com>
2023-12-03 12:33:45 -0300

    primeiro commit

---
+++ Untitled-2.js
@@ -0,0 +1,8 @@
+//saida
+console.log("Digita o nome do seu jogador")
+
+//variável
+let nickname = "Temito"
+
+//concantenando uma mensagem fixa com uma variável
+console.log("Bem Vindo" + nickname)
---
+++ XP do Herói.js
@@ -0,0 +1,15 @@
+// Declaração das variáveis
+const nome = "Temito";
+const xp = 1000000;
+
+// Estrutura de decisão
+const nivel = xp >= 10001 ? "Radiante" :
+xp >= 9001 ? "Imortal" :
+xp >= 8001 ? "Ascendente" :
+xp >= 7001 ? "Platina" :
+xp >= 6001 ? "Ouro" :
+xp >= 5001 ? "Prata" :
+xp >= 2001 ? "Bronze" : "Ferro";
+
+// Exibição da mensagem
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
